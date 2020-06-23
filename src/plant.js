5667918933ef52f6b82f5cce2f62c488738ac9a8
unknown <macjay314@gmail.com>
2020-06-23 14:34:01 -0700

    Add function factory and jquery to test changes

--- src/plant.js
+++ src/plant.js
@@ -1,23 +1,37 @@
-class Plant {
-  constructor() {
-    this.water = 0;
-    this.soil = 0;
-    this.light = 0;
+// This function stores our state.
+
+const storeState = () => {
+  let currentState = {};
+  return (stateChangeFunction) => {
+    const newState = stateChangeFunction(currentState);
+    currentState = {...newState};
+    return newState;
   }
-  
-  const changeState = (prop) => {
-    return (value) => {
-      return (state) => ({
-        ...state,
-        [prop] : (state[prop] || 0) + value
-      })
-    }
+}
+
+const stateChanger = storeState();
+
+// This is a function factory. We can easily create more specific functions that alter a plant's soil, water, and light to varying degrees.
+
+const changeState = (prop) => {
+  return (value) => {
+    return (state) => ({
+      ...state,
+      [prop] : (state[prop] || 0) + value
+    })
   }
+}
+
+// We create two functions using our function factory. We could easily create many more.
 
 const feed = changeState("soil");
-const hydrate = changeState("water");
-const giveLight = changeState("light");
-const blueFood = changeState("soil")(5)
-const greenFood = changeState("soil")(10)
-const yuckyFood = changeState("soil")(-5)
-}
+const blueFood = changeState("soil")(5);
+
+$(document).ready(function() {
+
+// This function has side effects because we are using jQuery. Manipulating the DOM will always be a side effect.
+  $('#feed').click(function() {
+    const newState = stateChanger(blueFood);
+    $('#soil-value').text(newState.soil);
+  });
+});
